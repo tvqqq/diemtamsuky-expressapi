57fab71b4696ad5fe78f6d6285b143aae3aabee2
Tat Vi Quyen <tvq9612@gmail.com>
2021-10-22 16:23:47 +0700

    index page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const JwtService = require("../app/services/jwt.service");
 const UserController = require("../app/controllers/user.controller");
 
 const route = (app) => {
+  app.get("/", (req, res) => {
+    res.status(200).send("<code>Hello from Điểm tâm Sú Ky Express API!</code>");
+  });
+
   // API for Zalo Router
   app.post("/api/login", UserController.login);
   app.use("/api", JwtService.verify, apiRouter);
